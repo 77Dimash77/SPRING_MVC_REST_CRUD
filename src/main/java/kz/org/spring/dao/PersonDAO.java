f5c7ff6e3fcef066b3630b9fef6eba0b3059a7ea
Dimash <mwasikzano@gmail.com>
2024-04-14 18:50:07 +0500

    SPRING REST CRUD 1

---
+++ src/main/java/kz/org/spring/dao/PersonDAO.java
@@ -0,0 +1,26 @@
+package kz.org.spring.dao;
+
+import kz.org.spring.models.Person;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+    private static int PEOPLECOUNT;
+    private List<Person> people;
+    {
+        people = new ArrayList<>();
+        people.add(new Person(++PEOPLECOUNT,"Tom"));
+        people.add(new Person(++PEOPLECOUNT,"Jerry"));
+        people.add(new Person(++PEOPLECOUNT,"Ushlep"));
+        people.add(new Person(++PEOPLECOUNT,"LOL"));
+    }
+    public List<Person> index(){
+        return people;
+    }
+    public Person show(int id){
+    return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+    }
+}
